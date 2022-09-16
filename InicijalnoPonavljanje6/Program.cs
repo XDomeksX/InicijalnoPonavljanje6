ba4b52f7f2eeee588efa221609cc97323ae88778
Dome <Dome@DESKTOP-7PFIV6R>
2022-09-16 14:59:06 +0200

    Add project files.

---
+++ InicijalnoPonavljanje6/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InicijalnoPonavljanje6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 1;
+            Console.WriteLine("Unesi broj članova reda: ");
+            int brojClanova = Convert.ToInt32(Console.ReadLine());
+
+            while (i <= brojClanova)
+            {
+                Console.Write(" + 1/" + Math.Pow(3, i));
+                i++;
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
